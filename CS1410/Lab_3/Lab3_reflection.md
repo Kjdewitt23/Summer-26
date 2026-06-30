ef54627c77afe17afdcd289730911f442f4a5582
Kjdewitt23 <kollin.dewitt@gmail.com>
2026-06-30 12:14:13 -0600

    Added reflections and updated youtube channel

---
+++ CS1410/Lab_3/Lab3_reflection.md
@@ -0,0 +1,3 @@
+1. Yes, you can access it directly. The single underscore just shows other engineers and your future self that you SHOULDN'T access it outside of the class.
+
+2. No, you can't it throws an AttributeError. You can access it with it's 'mangled' name that includes the class name. 
--- CS1410/Lab_3/youtube_channel.py
+++ CS1410/Lab_3/youtube_channel.py
@@ -10,15 +10,38 @@ class YouTubeChannel:
         name: the channel title  
         video_count: number of videos uploaded to this channel  
         """
-        self.name = name
-        self.video_count = video_count
+        self._name = name
+        self.__video_count = video_count
 
     def __str__(self) -> str:
-        return f"Channel: {self.name}, Videos: {self.video_count}"
+        return f"Channel: {self._name}, Videos: {self.__video_count}"
+    
+    def get_name(self) -> str:
+        return self._name
+    
+    def set_name(self, name) -> None:
+        self._name = name
+
+    def get_video_count(self) -> int:
+        return self.__video_count
+    
+    def set_video_count(self, video_count) -> None:
+        if video_count >= 0:
+            self.__video_count = video_count
+    
 
 def main():
     channel = YouTubeChannel("UVUCS1410", 150)
-    print(channel)
+    print(channel.get_name(), channel.get_video_count())
+    channel.set_name('Cool Channel')
+    channel.set_video_count(400)
+    print(channel.get_name(), channel.get_video_count())
+
+    print(channel._name) # For reflection question 1
+
+    #print(channel.__video_count) # For reflection question 2 - Throws error
+    print(channel._YouTubeChannel__video_count) # This works because it's calling the mangled name.
+
 
 if __name__ == "__main__":
     main()
